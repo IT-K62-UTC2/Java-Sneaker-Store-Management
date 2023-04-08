7fd079bb4e2f3688e2e158dc26cb638f53b5373e
Nguyen Dang Quy <dangquyitt@gmail.com>
2023-04-08 22:40:54 +0700

    update category repo

--- src/main/java/utc2/itk62/sneaker/repositories/CategoryRepo.java
+++ src/main/java/utc2/itk62/sneaker/repositories/CategoryRepo.java
@@ -41,5 +41,111 @@ public class CategoryRepo {
         }
     }
 
+    public Category getCategoryById(int id) {
+        String query = "SELECT * FROM category WHERE status = 1 AND id = ?";
+        try {
+            PreparedStatement ptmt = ConnectionUtil.getConnection().prepareStatement(query);
+            ptmt.setInt(1,  id);
+            ResultSet rs = ptmt.executeQuery();
+            Category category = null;
+            while (rs.next()) {
+                category = new Category();
+                category.setId(rs.getInt("id"));
+                category.setName(rs.getString("name"));
+                category.setStatus(rs.getInt("status"));
+                category.setCreatedAt(rs.getTimestamp("created_at"));
+                category.setUpdatedAt(rs.getTimestamp("updated_at"));
+            }
+            return category;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            ConnectionUtil.closeConnection();
+        }
+    }
 
+    public Category getCategoryByName(String name) {
+        String query = "SELECT * FROM category WHERE status = 1 LIKE '%'?'%'";
+        try {
+            PreparedStatement ptmt = ConnectionUtil.getConnection().prepareStatement(query);
+            ptmt.setString(1,  name);
+            ResultSet rs = ptmt.executeQuery();
+            Category category = null;
+            while (rs.next()) {
+                category = new Category();
+                category.setId(rs.getInt("id"));
+                category.setName(rs.getString("name"));
+                category.setStatus(rs.getInt("status"));
+                category.setCreatedAt(rs.getTimestamp("created_at"));
+                category.setUpdatedAt(rs.getTimestamp("updated_at"));
+            }
+            return category;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            ConnectionUtil.closeConnection();
+        }
+    }
+
+    public int updateCategory(Category category){
+        String query = "UPDATE category SET name = ? WHERE id = ?";
+        int result = 0;
+        Connection conn = ConnectionUtil.getConnection();
+        try{
+            conn.setAutoCommit(false);
+            PreparedStatement ptmt = conn.prepareStatement(query);
+            ptmt.setString(1, category.getName());
+            ptmt.setInt(2, category.getId());
+            result = ptmt.executeUpdate();
+            conn.commit();
+        }catch (SQLException e){
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            e.printStackTrace();
+        } finally{
+            ConnectionUtil.closeConnection();
+        }
+        return result;
+    }
+
+    public int createCategory(Category category) {
+        String query = "INSERT INTO category(name) VALUES(?)";
+        int result = 0;
+        Connection conn = ConnectionUtil.getConnection();
+
+        try{
+            conn.setAutoCommit(false);
+            PreparedStatement ptmt = conn.prepareStatement(query);
+            ptmt.setString(1, category.getName());
+            result = ptmt.executeUpdate();
+            conn.commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            ConnectionUtil.closeConnection();
+        }
+        return result;
+    }
+
+    public int deleteCategory(int categoryId) {
+        String query = "UPDATE category SET status = 0 WHERE id = ?";
+        int result = 0;
+        Connection conn = ConnectionUtil.getConnection();
+
+        try{
+            conn.setAutoCommit(false);
+            PreparedStatement ptmt = conn.prepareStatement(query);
+            ptmt.setInt(1, categoryId);
+            result = ptmt.executeUpdate();
+            conn.commit();
+        }catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            ConnectionUtil.closeConnection();
+        }
+        return result;
+    }
 }
