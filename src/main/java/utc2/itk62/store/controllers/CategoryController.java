fcadfb6bad662773c86019a03d265f10e3209761
Nguyen Dang Quy <dangquyitt@gmail.com>
2023-05-03 17:50:53 +0700

    optimize load product in category

--- src/main/java/utc2/itk62/store/controllers/CategoryController.java
+++ src/main/java/utc2/itk62/store/controllers/CategoryController.java
@@ -1,5 +1,6 @@
 package utc2.itk62.store.controllers;
 
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -198,21 +199,25 @@ public class CategoryController {
         if (!tableListCategory.getItems().isEmpty()) {
             tableListCategory.getItems().clear();
         }
-        categoryList = FXCollections.observableArrayList(categoryService.getAllCategory());
-        for (Category category : categoryList) {
-            category.setProductList(productService.getProductsByIdCategory(category.getId()));
-        }
-        colIdCategory.setCellValueFactory(new PropertyValueFactory<Category, Integer>("id"));
-        colNameCategory.setCellValueFactory(new PropertyValueFactory<Category, String>("name"));
-        colStatusCategory.setCellValueFactory(new PropertyValueFactory<Category, Integer>("status"));
-        colCreatedAtCategory.setCellValueFactory(new PropertyValueFactory<Category, Timestamp>("createdAt"));
-        colUpdatedAtCategory.setCellValueFactory(new PropertyValueFactory<Category, Timestamp>("updatedAt"));
-        tableListCategory.setItems(categoryList);
+        new Thread(()-> {
+            categoryList = FXCollections.observableArrayList(categoryService.getAllCategory());
+            for (Category category : categoryList) {
+                category.setProductList(productService.getProductsByIdCategory(category.getId()));
+            }
+            Platform.runLater(()->{
+                colIdCategory.setCellValueFactory(new PropertyValueFactory<Category, Integer>("id"));
+                colNameCategory.setCellValueFactory(new PropertyValueFactory<Category, String>("name"));
+                colStatusCategory.setCellValueFactory(new PropertyValueFactory<Category, Integer>("status"));
+                colCreatedAtCategory.setCellValueFactory(new PropertyValueFactory<Category, Timestamp>("createdAt"));
+                colUpdatedAtCategory.setCellValueFactory(new PropertyValueFactory<Category, Timestamp>("updatedAt"));
+                tableListCategory.setItems(categoryList);
 
-        // update table other
-        tableListCategory.getSelectionModel().selectFirst();
-        updateCurrentCategoryToForm();
-        updateProductCurrentRowCategory();
+                // update table other
+                tableListCategory.getSelectionModel().selectFirst();
+                updateCurrentCategoryToForm();
+                updateProductCurrentRowCategory();
+            });
+        }).start();
     }
 
     private void updateProductCurrentRowCategory() {
