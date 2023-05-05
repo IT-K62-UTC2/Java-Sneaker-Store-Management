099d59874816062359640b840fa006796b5831e3
ducphong <doducphong21062003@gmail.com>
2023-05-05 14:08:01 +0700

    add logo for btn in category

--- src/main/java/utc2/itk62/store/Main.java
+++ src/main/java/utc2/itk62/store/Main.java
@@ -3,6 +3,7 @@ package utc2.itk62.store;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import utc2.itk62.store.util.HashedPassword;
 
@@ -15,6 +16,7 @@ public class Main extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 1400, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/utc2/itk62/store/images/iconTile.png")));
         stage.show();
     }
 
